6f3d26850432fe4c1908360b3846a7aeb34fabf6
Piotr Mroczek <80900800+DemoKtr@users.noreply.github.com>
2024-05-13 12:48:08 +0200

    write gbuffer descriptor set

--- VulkanGame/Gbuffer.h
+++ VulkanGame/Gbuffer.h
@@ -4,4 +4,5 @@
 namespace vkGbuffer {
 	void createGBufferAttachments(vk::PhysicalDevice physicalDevice, vk::Device logicalDevice, vkUtil::GbufferAttachments* gbuffer);
 	void createAttachment(vk::PhysicalDevice physicalDevice, vk::Device logicalDevice, vk::Format format, vk::ImageUsageFlags usage, vkUtil::FrameBufferAttachment* attachment, int32_t width, int32_t height);
+	void make_descriptor_set(vk::Device logicalDevice, vk::DescriptorSet descriptorSet, vk::DescriptorPool descriptorPool, vk::DescriptorSetLayout layout, vkUtil::GbufferAttachments gbuffer);
 }
