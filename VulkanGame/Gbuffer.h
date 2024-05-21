976d3ebf7776458fe927c6cd2eee7f1957a7e076
Piotr Mroczek <80900800+DemoKtr@users.noreply.github.com>
2024-05-21 23:18:38 +0200

    git fix

--- VulkanGame/Gbuffer.h
+++ VulkanGame/Gbuffer.h
@@ -1,5 +1,18 @@
 #pragma once
 #include "config.h"
 #include "RenderStruct.h"
+
+struct attachmentBundle {
+	vk::Format format;
+	vk::ImageUsageFlagBits usage;
+	vkUtil::FrameBufferAttachment* attachment;
+	int32_t width, height;
+	vk::Device logicalDevice;
+	vk::PhysicalDevice physicalDevice;
+};
+
 namespace vkGbuffer {
+	void createAttachment(attachmentBundle attachmentDescription);
+	void createGbufferAttachment(vk::PhysicalDevice physicalDevice, vk::Device logicalDevice, vkUtil::Gbuffer* gbuffer);
+	void writeGbufferDescriptor(vk::DescriptorSet descriptorSet, vk::Device logicalDevice, vkUtil::Gbuffer gBuffer);
 }
